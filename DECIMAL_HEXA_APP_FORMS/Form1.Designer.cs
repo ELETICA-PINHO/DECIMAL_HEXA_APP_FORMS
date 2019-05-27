7dfc8e38c9bef1fb12b2c43c6fff89a5c788a302
eletricapinho <eletricapinho@outlook.com>
2019-05-27 08:57:57 -0300

    atualzação de componentes porem com erro numero 256

--- DECIMAL_HEXA_APP_FORMS/Form1.Designer.cs
+++ DECIMAL_HEXA_APP_FORMS/Form1.Designer.cs
@@ -29,96 +29,84 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Decimal = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txt_Decimal = new System.Windows.Forms.TextBox();
-            this.lbl_Numero_Convertido_hexa = new System.Windows.Forms.Label();
-            this.lbl_convert = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label_Numero_hexa = new System.Windows.Forms.Label();
+            this.button_convert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(2, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(496, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "CONVERSOR DE DECIMAL PARA HEXADECIMAL";
             // 
+            // textBox_Decimal
+            // 
+            this.textBox_Decimal.Location = new System.Drawing.Point(176, 91);
+            this.textBox_Decimal.Name = "textBox_Decimal";
+            this.textBox_Decimal.Size = new System.Drawing.Size(132, 20);
+            this.textBox_Decimal.TabIndex = 1;
+            this.textBox_Decimal.TextChanged += new System.EventHandler(this.textBox_Decimal_TextChanged);
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(142, 64);
+            this.label2.Location = new System.Drawing.Point(173, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 19);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "NUMERO DECIMAL 0 A 300";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // txt_Decimal
-            // 
-            this.txt_Decimal.Location = new System.Drawing.Point(165, 101);
-            this.txt_Decimal.Multiline = true;
-            this.txt_Decimal.Name = "txt_Decimal";
-            this.txt_Decimal.Size = new System.Drawing.Size(152, 20);
-            this.txt_Decimal.TabIndex = 2;
-            this.txt_Decimal.TextChanged += new System.EventHandler(this.txt_Decimal_TextChanged);
-            // 
-            // lbl_Numero_Convertido_hexa
-            // 
-            this.lbl_Numero_Convertido_hexa.AutoSize = true;
-            this.lbl_Numero_Convertido_hexa.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Numero_Convertido_hexa.ForeColor = System.Drawing.Color.Red;
-            this.lbl_Numero_Convertido_hexa.Location = new System.Drawing.Point(190, 159);
-            this.lbl_Numero_Convertido_hexa.Name = "lbl_Numero_Convertido_hexa";
-            this.lbl_Numero_Convertido_hexa.Size = new System.Drawing.Size(0, 32);
-            this.lbl_Numero_Convertido_hexa.TabIndex = 3;
-            // 
-            // lbl_convert
-            // 
-            this.lbl_convert.AutoSize = true;
-            this.lbl_convert.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_convert.ForeColor = System.Drawing.Color.Red;
-            this.lbl_convert.Location = new System.Drawing.Point(215, 165);
-            this.lbl_convert.Name = "lbl_convert";
-            this.lbl_convert.Size = new System.Drawing.Size(28, 24);
-            this.lbl_convert.TabIndex = 4;
-            this.lbl_convert.Text = "...";
-            this.lbl_convert.Click += new System.EventHandler(this.lbl_convert_Click);
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digitar numero de 0 ha 300";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(118, 173);
+            this.label3.Location = new System.Drawing.Point(131, 177);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(72, 13);
-            this.label3.TabIndex = 5;
+            this.label3.TabIndex = 3;
             this.label3.Text = "Numero Hexa";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label_Numero_hexa
+            // 
+            this.label_Numero_hexa.AutoSize = true;
+            this.label_Numero_hexa.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Numero_hexa.Location = new System.Drawing.Point(219, 170);
+            this.label_Numero_hexa.Name = "label_Numero_hexa";
+            this.label_Numero_hexa.Size = new System.Drawing.Size(25, 22);
+            this.label_Numero_hexa.TabIndex = 4;
+            this.label_Numero_hexa.Text = "...";
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(361, 122);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(75, 23);
+            this.button_convert.TabIndex = 5;
+            this.button_convert.Text = "Convert";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(509, 450);
+            this.ClientSize = new System.Drawing.Size(505, 450);
+            this.Controls.Add(this.button_convert);
+            this.Controls.Add(this.label_Numero_hexa);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.lbl_convert);
-            this.Controls.Add(this.lbl_Numero_Convertido_hexa);
-            this.Controls.Add(this.txt_Decimal);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Decimal);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(585, 489);
-            this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(0, 489);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimumSize = new System.Drawing.Size(525, 39);
             this.Name = "Form1";
-            this.TransparencyKey = System.Drawing.Color.Transparent;
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +115,11 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Decimal;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txt_Decimal;
-        private System.Windows.Forms.Label lbl_Numero_Convertido_hexa;
-        private System.Windows.Forms.Label lbl_convert;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_Numero_hexa;
+        private System.Windows.Forms.Button button_convert;
     }
 }
 
--- DECIMAL_HEXA_APP_FORMS/Form1.cs
+++ DECIMAL_HEXA_APP_FORMS/Form1.cs
@@ -17,34 +17,90 @@ namespace DECIMAL_HEXA_APP_FORMS
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        
+        int Ndecimal;
+        int Nhexa;
+        char[] vetor_Hexa = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        char convertQ = ' ';
+        char convertR = ' ';
+        string convert = " ";
+        int convert_quociente = 0;
+        int convert_resto = 0;
+        int N_digitado = 0;
+        int base_divisor = 16;
+        char resto1 = ' ';
+        int quociente1 = 0;
+
+
+        
+
+
+
+
+        private void textBox_Decimal_TextChanged(object sender, EventArgs e)
         {
+            N_digitado = int.Parse(textBox_Decimal.Text);
+
 
         }
 
-        private void txt_Decimal_TextChanged(object sender, EventArgs e)
+        private void button_convert_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (N_digitado <= 0 || N_digitado <= 300)  // condição numero
+                {
+                    convert_quociente = N_digitado / base_divisor;
+                    convert_resto = N_digitado % base_divisor;
+
+                    if (N_digitado <= 256) // ATE 256 DECIMAIS
+                    {
+                        convertQ = vetor_Hexa[convert_quociente];   //VETOR CONVERSÃO
+                        convertR = vetor_Hexa[convert_resto];       //VETOR CONVERSÃO
+                        convert = Convert.ToString(convertQ) + Convert.ToString(convertR);  //MONTAGEM HEXA AT 256
+                       
+                    }
 
 
 
+                    if (N_digitado > 256)
+                    {
+                        quociente1 = convert_quociente;
+                        resto1 = vetor_Hexa[convert_resto];  //menos sequinativo 3
+
+                        if (convert_quociente >= 16)
+                        {
+                            convertQ = vetor_Hexa[quociente1 / base_divisor];  ////menos sequinativo 1
+                            convertR = vetor_Hexa[quociente1 % base_divisor];  ////menos sequinativo 2
+
+                        }
+
+                        convert = Convert.ToString(convertQ) + Convert.ToString(convertR) + Convert.ToString(resto1);
+                        //                                1                            2                             3
+
+                    }
 
 
+                    label_Numero_hexa.Text = convert.ToString();
 
 
-        }
+                } // if 0 300
+                else
+                {
+                    MessageBox.Show("Valor não é permitido tente novamente, 0 a 300");
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+                }
+               
 
-        }
 
-        private void lbl_convert_Click(object sender, EventArgs e)
-        {
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro");
 
-        }
+            }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
+
 
         }
     }
